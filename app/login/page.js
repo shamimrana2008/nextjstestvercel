d7f659048d57df9b1f100d94e5221e1b902d57c1
shamimranapro85 <shamimranaprofessional@gmail.com>
2024-10-17 14:52:06 +0600

    test next js deploying

---
+++ app/login/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-center text-6xl text-red-500'>Lgoin </h1>
+
+    </div>
+  )
+}
+
+export default page
